b81f4c45f2a5f52a0dbf3a4c550a07bcbd8959ee
jstogasterisk <111814819+jstogasterisk@users.noreply.github.com>
2024-06-27 12:31:04 -0400

    Update josharch.sh

--- josharch.sh
+++ josharch.sh
@@ -1,8 +1,22 @@
-message="Hello and welcome! This is the quick way of getting Josh's Arch Linux set up running!"
-delay=0.05
+#!/bin/bash
 
-for (( i=0; i<${#message}; i++ )); do
-  echo -n "${message:$i:1}"
+part1="Hello and welcome!"
+part2=" This is the quick way of getting Josh's Arch Linux set up running!"
+delay=0.05
+pause=1
+
+# Type out the first part of the message
+for (( i=0; i<${#part1}; i++ )); do
+  echo -n "${part1:$i:1}"
+  sleep $delay
+done
+
+# Add a pause before the second part
+sleep $pause
+
+# Type out the second part of the message
+for (( i=0; i<${#part2}; i++ )); do
+  echo -n "${part2:$i:1}"
   sleep $delay
 done
 
