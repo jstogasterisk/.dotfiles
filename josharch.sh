b439918a3d0b9cf714808f7dd76a7c8000fadb52
jstogasterisk <111814819+jstogasterisk@users.noreply.github.com>
2024-06-27 12:57:32 -0400

    Update josharch.sh

--- josharch.sh
+++ josharch.sh
@@ -2,8 +2,10 @@
 
 part1="Hello and welcome!"
 part2=" This is the quick way of getting Josh's Arch Linux system up and running!"
-part3=" First, we need to create your file system."
-part4=" File system structure created."
+part3="First, we need to create your file system."
+part4="File system structure created."
+part5="Great! Now we can install some necessary packages."
+part6="Do you want to continue with the installation? [Y/n]"
 delay=0.05
 pause=1
 
@@ -49,4 +51,40 @@ for (( i=0; i<${#part4}; i++ )); do
   sleep $delay
 done
 
+# Add a pause before the fifth part
+sleep $pause
+
+# Move to a new line and type out the fifth part of the message
 echo
+for (( i=0; i<${#part5}; i++ )); do
+  echo -n "${part5:$i:1}"
+  sleep $delay
+done
+
+# Add a pause before the sixth part
+sleep $pause
+
+# Move to a new line and type out the sixth part of the message
+echo
+for (( i=0; i<${#part6}; i++ )); do
+  echo -n "${part6:$i:1}"
+  sleep $delay
+done
+
+# Get user input
+echo
+read -p "Do you want to continue with the installation? [Y/n] " -n 1 -r
+echo
+
+# Set default to 'y' if no input is provided
+if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
+  sudo pacman -S cmatrix
+else
+  part7="No worries!"
+  echo
+  for (( i=0; i<${#part7}; i++ )); do
+    echo -n "${part7:$i:1}"
+    sleep $delay
+  done
+  echo
+fi
