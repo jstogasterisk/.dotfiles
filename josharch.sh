069247543de6cf7155b5ee6d863820c921b03841
jstogasterisk <111814819+jstogasterisk@users.noreply.github.com>
2024-06-27 12:37:11 -0400

    Update josharch.sh

--- josharch.sh
+++ josharch.sh
@@ -2,6 +2,7 @@
 
 part1="Hello and welcome!"
 part2=" This is the quick way of getting Josh's Arch Linux system up and running!"
+part3=" First, we need to create your file system."
 delay=0.05
 pause=1
 
@@ -20,4 +21,21 @@ for (( i=0; i<${#part2}; i++ )); do
   sleep $delay
 done
 
+# Add a pause before the third part
+sleep $pause
+
+# Move to a new line and type out the third part of the message
 echo
+for (( i=0; i<${#part3}; i++ )); do
+  echo -n "${part3:$i:1}"
+  sleep $delay
+done
+
+# Perform the commands
+sleep $pause
+cd
+mkdir -p testfolder/Documents testfolder/Downloads testfolder/Pictures
+cd testfolder/Pictures
+
+echo
+echo "File system structure created."
