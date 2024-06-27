7a7ef1b9bdaf3e967a2c0e62a724ab66823b5d0b
jstogasterisk <111814819+jstogasterisk@users.noreply.github.com>
2024-06-27 13:05:43 -0400

    Update josharch.sh

--- josharch.sh
+++ josharch.sh
@@ -3,11 +3,13 @@
 part1="Hello and welcome!"
 part2=" This is the quick way of getting Josh's Arch Linux system up and running!"
 part3="First, we need to create your file system."
+creating="Creating file system"
 part4="File system structure created."
 part5="Great! Now we can install some necessary packages."
-part6="Do you want to continue with the installation? [Y/n]"
+part6="No worries!"
 delay=0.05
 pause=1
+dot_delay=0.5
 
 # Type out the first part of the message
 for (( i=0; i<${#part1}; i++ )); do
@@ -34,14 +36,23 @@ for (( i=0; i<${#part3}; i++ )); do
   sleep $delay
 done
 
-# Perform the commands
+# Add a pause before "Creating file system"
 sleep $pause
-cd
-mkdir -p testfolder/Documents testfolder/Downloads testfolder/Pictures
-cd testfolder/Pictures
-mkdir Wallpapers
 
-# Add a pause before the fourth part
+# Move to a new line and type out "Creating file system"
+echo
+for (( i=0; i<${#creating}; i++ )); do
+  echo -n "${creating:$i:1}"
+  sleep $delay
+done
+
+# Add the ellipsis
+for (( i=0; i<3; i++ )); do
+  echo -n "."
+  sleep $dot_delay
+done
+
+# Pause before part4
 sleep $pause
 
 # Move to a new line and type out the fourth part of the message
@@ -51,6 +62,13 @@ for (( i=0; i<${#part4}; i++ )); do
   sleep $delay
 done
 
+# Perform the commands
+sleep $pause
+cd
+mkdir -p testfolder/Documents testfolder/Downloads testfolder/Pictures
+cd testfolder/Pictures
+mkdir Wallpapers
+
 # Add a pause before the fifth part
 sleep $pause
 
@@ -58,16 +76,6 @@ sleep $pause
 echo
 for (( i=0; i<${#part5}; i++ )); do
   echo -n "${part5:$i:1}"
-  sleep $delay
-done
-
-# Add a pause before the sixth part
-sleep $pause
-
-# Move to a new line and type out the sixth part of the message
-echo
-for (( i=0; i<${#part6}; i++ )); do
-  echo -n "${part6:$i:1}"
   sleep $delay
 done
 
@@ -80,10 +88,9 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
   sudo pacman -S cmatrix
 else
-  part7="No worries!"
   echo
-  for (( i=0; i<${#part7}; i++ )); do
-    echo -n "${part7:$i:1}"
+  for (( i=0; i<${#part6}; i++ )); do
+    echo -n "${part6:$i:1}"
     sleep $delay
   done
   echo
